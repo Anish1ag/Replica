ebe7d48c56c2e6bda197e9bed7e478fdbc7b73fb
Anish1ag <anisgho13@gmail.com>
2024-02-06 17:00:05 +0545

    Replicate project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Component from "./component/Component";
+
+function App() {
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Component.jsx
@@ -0,0 +1,17 @@
+import "./component.css";
+function Component() {
+  return (
+    <>
+      <div className="instagram">
+        <div className="insta">
+          <img src="phone.png" alt="" />
+        </div>
+        <div className="cover">
+          <img src="cover.png" alt="" height={"290px"} width={"416px"} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Component;
